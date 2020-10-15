fe64b658b529144d1476a2daf2e771b0d8533dae
Timothy Young <tyoung5194@gmail.com>
2020-10-15 19:10:08 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,18 +1,9 @@
 function basicTeenager(age) {
-  if (age>=13 &&& age<=19) {
-    return(`You are a teenager!`)
-  } else if (age<=12 || age>19) {
-    return(`You are not a teenager.`)
-  }
+
 }
 
 function teenager(age) {
-  if (13<=age || 19>=age) {
-  return(`You are a teenager!`)
-} else if (age<=12 || age>=19) {
-  return(`You are not a teenager`)
-} else if (age>19)
-  return (`you are a grownup`)
+
 }
 function ageChecker(age) {
 
