bf633da416b73b0b0398c08ee1a8e0759dac052c
Timothy Young <tyoung5194@gmail.com>
2020-10-15 18:53:58 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -3,7 +3,7 @@ return(`You are a teenager!`)
 }
 
 function teenager(age) {
-if(true) { return(`You are a teenager`)}
+if(true) { return(`You are a teenager!`)}
 else {
   return(`You are not a teenager`)
 }
